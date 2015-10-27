25155b01a7f81713169f096bd628ef9c110f981b
Mina <wilhelmina.jane@gmail.com>
2015-10-27 18:26:39 +0000

    data saves into db, timestamps not functional

--- server.js
+++ server.js
@@ -3,20 +3,26 @@ var fs = require('fs');
 var jsonfile = require('jsonfile');
 var url = require('url');
 var querystring = require('querystring');
+var blogDataStringified = fs.readFileSync('./blog.json', 'utf-8');
+var blogData = JSON.parse(blogDataStringified);
 
 var server = http.createServer(function (request, response) {
+    console.log(blogData);
 
     if (request.method === 'GET') {
         var path;
 
         if (request.url === '/posts') {
-            path = './blog.json';
+            // get posts
         } else if (request.url.match('/public')) {
             path = "." + request.url;
         } else {
             path = './public/index.html';
         }
+        // read your file from hard drive
+        console.log(">>>>", path);
         fs.readFile(path, 'utf-8', function(err, file) {
+            // put in your response to send back to client
             response.write(file);
             response.end();
         });
@@ -24,22 +30,28 @@ var server = http.createServer(function (request, response) {
     if (request.method === 'POST') {
         // add a post
         var collection = '';
+        var blogpost;
         request.on('data', function(data) {
             collection += data;
+
         });
+
         request.on('end', function() {
-            collection = querystring.parse(collection);
-            console.log(collection);
-            // write to json
-            // redirects
-            response.end();
+            blogpost = querystring.parse(collection);
+            console.log(blogpost);
+            blogData[Date.now()] = blogpost.entry;
+            fs.writeFile('./blog.json', JSON.stringify(blogpost), function(err) {
+                if (err) {
+                    console.log(err);
+                }
+                response.end();
+            });
+
         });
-
-
 
     }
 
 });
 
-server.listen(3000);
+server.listen(8000);
 console.log("blog server is running!");
