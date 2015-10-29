ac3b15e0bc764e3e4147bfb0acf31400836a951a
Mina <wilhelmina.jane@gmail.com>
2015-10-29 23:30:49 +0000

    blogposts loading onto dom

--- public/script.js
+++ public/script.js
@@ -1,10 +1,20 @@
-console.log("hey");
 $(document).ready(function() {
     $.ajax({
         url: '/posts',
         dataType: 'json',
         success: function(data) {
             console.log(data);
+            for (var blogPost in data) {
+                var postDiv = document.createElement('div');
+                postDiv.className = "post";
+                var blogText = document.createTextNode(data[blogPost]);
+                postDiv.appendChild(blogText);
+
+                var blogPostArea = document.getElementById('posts');
+                blogPostArea.appendChild(postDiv);
+            }
+
+
         },
         error: function(error){
             console.log(error);
