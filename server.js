ae6da53425cffd4836eabbdc343d5eacc9bccc68
Mina <wilhelmina.jane@gmail.com>
2015-10-29 23:19:20 +0000

    blog posts save to a json

--- server.js
+++ server.js
@@ -3,55 +3,69 @@ var fs = require('fs');
 var jsonfile = require('jsonfile');
 var url = require('url');
 var querystring = require('querystring');
-var blogDataStringified = fs.readFileSync('./blog.json', 'utf-8');
-var blogData = JSON.parse(blogDataStringified);
+var blogDataStringified;
+var blogData;
 
 var server = http.createServer(function (request, response) {
-    console.log(blogData);
 
     if (request.method === 'GET') {
+
         var path;
 
         if (request.url === '/posts') {
-            // get posts
+
+            // read posts from hard drive
+            fs.readFile('./blog.json', 'utf-8', function(err, data) {
+                console.log (data);
+                // save as a JavaScript object
+                blogData = JSON.parse(data);
+                console.log("after parsing blogData is a ", typeof blogData);
+                // send posts back to client
+                response.writeHead(200);
+                response.write(data);
+                response.end();
+            });
+
+
+
         } else if (request.url.match('/public')) {
             path = "." + request.url;
         } else {
             path = './public/index.html';
         }
-        // read your file from hard drive
-        console.log(">>>>", path);
-        fs.readFile(path, 'utf-8', function(err, file) {
-            // put in your response to send back to client
-            response.write(file);
-            response.end();
-        });
+        if (path){
+            fs.readFile(path, 'utf-8', function(err, file) {
+                // put in your response to send back to client
+                response.write(file);
+                response.end();
+            });
+        }
+
     }
     if (request.method === 'POST') {
         // add a post
-        var collection = '';
+        var data = '';
         var blogpost;
-        request.on('data', function(data) {
-            collection += data;
 
+        request.on('data', function(chunk) {
+            data += chunk;
         });
 
         request.on('end', function() {
-            blogpost = querystring.parse(collection);
-            console.log(blogpost);
-            blogData[Date.now()] = blogpost.entry;
-            fs.writeFile('./blog.json', JSON.stringify(blogpost), function(err) {
+            // extract relevant info from the request
+            blogpost = querystring.parse(data);
+            var timestamp = Date.now();
+            blogData[timestamp] = blogpost.entry;
+            fs.writeFile('./blog.json', JSON.stringify(blogData, null, 4), function(err) {
+
                 if (err) {
                     console.log(err);
                 }
                 response.end();
             });
-
         });
-
     }
-
 });
 
 server.listen(8000);
-console.log("blog server is running!");
+console.log("blog server is running at port 8000!");
