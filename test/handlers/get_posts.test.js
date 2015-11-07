9ef88c6cbafbb7d052f53f5ac39346f3dd918107
NataliaLKB <natalialeebaltazar@gmail.com>
2015-11-07 00:02:50 +0000

    adding writeFakeData and emptyData functions to test

--- test/handlers/get_posts.test.js
+++ test/handlers/get_posts.test.js
@@ -1,8 +1,32 @@
 "use strict";
 
+var fs          = require("fs");
 var test        = require("tape");
 var Shot        = require("shot");
 var getPosts    = require("../../handlers.js").getPosts
+
+function writeFakeData (callback) {
+    var time    = Date.now();
+    var posts   = {};
+    posts[time] = "something here";
+
+    fs.writeFile("./blog.json", JSON.stringify(posts, null, 4), function (err) {
+
+        if (err) {
+            return err
+        }
+
+        callback;
+    });
+}
+
+function emptyData () {
+    fs.writeFile("./blog.json", JSON.stringify({}, null, 4), function (err) {
+        if (err) {
+            return err;
+        }
+    });
+}
 
 test("/ serves up index.html", function (t) {
 
@@ -10,14 +34,15 @@ test("/ serves up index.html", function (t) {
         method: "GET",
         url: "/posts"
     }
+    writeFakeData(
+        Shot.inject(getPosts, options, function (res) {
+            // payload is always a string in the response, needs to be parsed
+            var payload = JSON.parse(res.payload)
 
-    Shot.inject(getPosts, options, function (res) {
-        // payload is always a string in the response, needs to be parsed
-        var payload = res.payload.length === 0
-                    ? {} // empty objects in the payload just come back as an empty string
-                    : JSON.parse(res.payload)
-
-        t.equal(typeof payload, "object", "Parsed payload comes back as an object");
-        t.end();
-    });
+            t.equal(res.statusCode, 200, "Congrats, response is okay");
+            t.equal(typeof payload, "object", "Parsed payload comes back as an object");
+            t.end();
+            emptyData();
+        })
+    )
 });
